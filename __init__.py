06c337a29ee43be1b573ceab07bf674af47f67dd
Sergey Ustyantsev <ustyantsev@gmail.com>
2020-05-17 16:10:38 +0300

    structure of project

---
+++ __init__.py
@@ -0,0 +1,10 @@
+ # @Author : schokoro
+# @Datetime : 2020/5/17 16:03
+# @File : __init__.py
+# @Last Modify Time : 2020/5/17 16:04
+# @Contact : ustyantsev@gmail.com
+
+"""
+    FILE :  __init__.py
+    FUNCTION : None
+"""
